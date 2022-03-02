95572e19fb2b56d5556a676f7bd4fa12d1e1ba2e
Trintos <trintos@live.ru>
2022-03-02 23:54:49 +0300

    add task 01

---
+++ Introduction_to_C#/Lesson1/Task00/Program.cs
@@ -0,0 +1,4 @@
+﻿//Напишите программу, которая на вход принимает число и выдаёт его квадрат (число умноженное на само себя)
+Console.WriteLine("Введите первое число:");
+int FirstValue = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine((int)(FirstValue * FirstValue));
---
+++ Introduction_to_C#/Lesson1/Task01/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго.
+Console.WriteLine("Введите первое число:");
+int FirstNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число:");
+int SecondNumber = Convert.ToInt32(Console.ReadLine());
+
+if (FirstNumber * FirstNumber == SecondNumber){
+  Console.WriteLine("Второе число является квадратом первого");
+
+}
+else{
+  Console.WriteLine("Второе число не является квадратом первого");
+}
