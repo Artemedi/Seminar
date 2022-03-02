dd79c755692dd4a6e50c10622d82758f2c35a4fe
Trintos <trintos@live.ru>
2022-03-03 00:46:18 +0300

    add task 04

---
+++ Introduction_to_C#/Lesson1/Task04/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая на вход принимает три числа и выдаёт самое максимальное.
+Console.WriteLine("Введите первое число:");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число:");
+int B = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число:");
+int C = Convert.ToInt32(Console.ReadLine());
+int MaxValue = A;
+
+if (MaxValue < B) MaxValue = B;
+if (MaxValue < C) MaxValue = C;
+
+Console.Write("Максимальное число ");
+Console.Write(MaxValue);
