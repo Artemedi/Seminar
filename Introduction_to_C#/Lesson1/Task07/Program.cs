d9d6b4c996bf49584306cb8dc332a3977068fb9d
Trintos <trintos@live.ru>
2022-03-03 01:17:07 +0300

    add task 07

---
+++ Introduction_to_C#/Lesson1/Task07/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные чила от 1 до N.
+Console.WriteLine( "Get number N" );
+int N = Convert.ToInt32(Console.ReadLine());
+int N1 = 1; 
+
+while (N1 <= N)
+{
+    if (N1 % 2 == 0)
+    {
+        Console.Write(N1 + " ");
+        N1 += 1;
+    }
+    else 
+    {
+        N1 += 1;
+    }
+}
