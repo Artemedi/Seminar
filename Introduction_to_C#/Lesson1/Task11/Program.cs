616d565816e16ae181558228447907987baeff29
Trintos <trintos@live.ru>
2022-03-07 18:37:09 +0300

    add task 11

---
+++ Introduction_to_C#/Lesson1/Task11/Program.cs
@@ -0,0 +1,19 @@
+﻿//Задача 11. Дано число из отрезка [10, 99]. Показать наибольшую цифру числа
+
+ Console.WriteLine("Ввести число в диапазоне от 10 до 99:");
+ int numberN = Convert.ToInt32(Console.ReadLine());
+ if (numberN >= 10 & numberN <= 99) 
+ {
+   int FirstDigit = numberN % 10;
+   int SecondDigit = numberN / 10;
+   if (FirstDigit == SecondDigit)
+   {
+     Console.WriteLine("Числа равны");
+   } else if (FirstDigit > SecondDigit)
+   {
+     Console.WriteLine("Второе наибольшее");
+   }else{
+     Console.WriteLine("Первое наибольшее");
+   }
+
+ }
