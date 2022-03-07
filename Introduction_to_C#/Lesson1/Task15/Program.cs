90c06e3c5c6b8399597df2d66f1eebeed56d809a
Trintos <trintos@live.ru>
2022-03-07 19:22:52 +0300

    add task 15

---
+++ Introduction_to_C#/Lesson1/Task15/Program.cs
@@ -0,0 +1,16 @@
+﻿// Дано число, проверить кратно ли оно 7 или 23
+
+int Digit = new Random().Next(10,1000);
+Console.WriteLine(Digit);
+int[] Massive = {7,23};
+foreach(int number in Massive)
+{
+    if ((Digit % number) == 0)
+{
+        Console.WriteLine($"Число {Digit} кратно {number}");
+}
+    else
+{
+        Console.WriteLine($"Число {Digit} не кратно {number}");
+}
+}
