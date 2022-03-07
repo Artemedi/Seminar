9bd7469c53a5a77df13d5a7db5f3c8257948394e
Trintos <trintos@live.ru>
2022-03-07 19:00:56 +0300

    add task 13

---
+++ Introduction_to_C#/Lesson1/Task13/Program.cs
@@ -0,0 +1,13 @@
+﻿// Выяснить, кратное ли число заданному, если нет, вывести остаток
+Console.WriteLine("Введите первое число");
+int Digit1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int Digit2 = Convert.ToInt32(Console.ReadLine());
+if ((Digit1 % Digit2)==0)
+{
+    Console.WriteLine("Числа кратные");
+}
+else
+{
+    Console.WriteLine($"Числа не кратные, остаток {(Digit1 % Digit2)}");
+}
