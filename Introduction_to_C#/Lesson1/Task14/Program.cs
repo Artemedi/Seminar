ed4feedb743d2608d61ce4808567d0e0cdd474b0
Trintos <trintos@live.ru>
2022-03-07 19:06:06 +0300

    add task 14

---
+++ Introduction_to_C#/Lesson1/Task14/Program.cs
@@ -0,0 +1,15 @@
+﻿// Найти третью цифру числа или сообщить, что её нет
+
+int Number = 0;
+Console.WriteLine("Get number ");
+Number = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+if (Number >= 100 | Number < 99) 
+{
+    Number /= 100;
+    int ThirdDigit = Number % 10;
+    Console.WriteLine("Number of third Digit  is " + ThirdDigit);
+} 
+else 
+{
+    Console.WriteLine("Number does't have third digit");
+}
