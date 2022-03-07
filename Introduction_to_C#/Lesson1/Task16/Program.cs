e3b2658f8a61e3424440432b360344437eecc9fc
Trintos <trintos@live.ru>
2022-03-07 19:24:46 +0300

    add task 16

---
+++ Introduction_to_C#/Lesson1/Task16/Program.cs
@@ -0,0 +1,10 @@
+﻿// Дано число обозначающее день недели. Выяснить является ли номер дня недели выходным
+
+Console.WriteLine("Print number (1-7)");
+string b = Console.ReadLine();
+if (b == "6" | b == "7") {
+  Console.WriteLine("weekend");
+}
+if (b != "6" & b != "7") {
+  Console.WriteLine("not a weeknd");
+}
