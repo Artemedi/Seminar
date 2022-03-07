1d93fc1e3391ed5100d284cf101372b925f6a238
Trintos <trintos@live.ru>
2022-03-07 18:38:55 +0300

    add task 12

---
+++ Introduction_to_C#/Lesson1/Task12/Program.cs
@@ -0,0 +1,7 @@
+﻿//12 Удалить вторую цифру трёхзначного числа
+
+int a = new Random().Next(100,1000);
+Console.WriteLine(a);
+int first = a / 100;
+int last = a % 10;
+Console.WriteLine($"{first}, {last}");
