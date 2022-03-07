e14b3c12fb9c467c5635789088edd7216bbce32f
Trintos <trintos@live.ru>
2022-03-07 18:51:51 +0300

    add task 10

---
+++ Introduction_to_C#/Lesson1/Task10/Program.cs
@@ -0,0 +1,5 @@
+﻿// Напишите программу, которая принимает на вход трехзначное число и на выходе показывает вторую цифру этого числа
+int Digit = new Random().Next(100,1000);
+Console.WriteLine($"Дано число {Digit}");
+int FirstDigit = (Digit % 100) / 10;
+Console.WriteLine($"Вторая цифра {FirstDigit}");
