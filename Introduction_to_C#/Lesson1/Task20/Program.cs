3c179adbd3adca37e62a4f47fbf361db9aee20a9
Trintos <trintos@live.ru>
2022-03-10 00:05:33 +0300

    add task 20 Two dots in 2D

--- Introduction_to_C#/Lesson1/Task20/Program.cs
+++ Introduction_to_C#/Lesson1/Task20/Program.cs
@@ -1,16 +1,14 @@
-﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 32 пространстве
 
 Console.WriteLine("Координаты первой точки");
 
 int x1 = Convert.ToInt32(Console.ReadLine());
 int y1 = Convert.ToInt32(Console.ReadLine());
-int z1 = Convert.ToInt32(Console.ReadLine());
 
 Console.WriteLine("Координаты второй точки");
 
 int x2 = Convert.ToInt32(Console.ReadLine());
 int y2 = Convert.ToInt32(Console.ReadLine());
-int z2 = Convert.ToInt32(Console.ReadLine());
 
-double S = Math.Round(Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2)),3);
+double S = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
 Console.WriteLine(S);
