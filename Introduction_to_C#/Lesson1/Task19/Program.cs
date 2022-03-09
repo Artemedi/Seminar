a345322d92421e87e49e93d935d59c4b0e37a8d1
Trintos <trintos@live.ru>
2022-03-09 23:39:04 +0300

    add task 19 Palindrome

--- Introduction_to_C#/Lesson1/Task19/Program.cs
+++ Introduction_to_C#/Lesson1/Task19/Program.cs
@@ -3,7 +3,7 @@ Console.Write("Введите пятизначное число: ");
 int Digit = Convert.ToInt32(Console.ReadLine());
 if ((Convert.ToString(Digit).Length) == 5)
 {
-    if ((Digit % 10)==(Digit / 10000)&(Digit % 100 / 10)==(Digit / 1000 % 10))
+    if ((Digit % 10)==(Digit / 10000) & (Digit % 100 / 10)==(Digit / 1000 % 10))
     // Беру последний символ Digit % 10 и сравниваю его с первым символом Digit / 10000
     // Беру два последних и усекаю до первого (Digit % 100 / 10) и сравниваю так же со вторым символом (Digit / 1000 % 10))
     {
