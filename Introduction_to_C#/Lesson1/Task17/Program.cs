a98b6a05d21b5d895b647e7e019efe7c8b1f68e5
Trintos <trintos@live.ru>
2022-03-09 21:30:44 +0300

    add task 17

---
+++ Introduction_to_C#/Lesson1/Task17/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу, которая принимает на вход координаты точки (X и Y), причём X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости, в которой находится эта точка.
+
+Console.WriteLine("input x");
+
+int x = int.Parse(Console.ReadLine());
+Console.WriteLine("input y");
+int y = int.Parse(Console.ReadLine());
+
+if (x > 0 & y > 0)
+{
+Console.WriteLine("first quarter");
+}
+else if (x < 0 & y > 0 )
+{
+Console.WriteLine("second quarter");
+}
+else if (x<0 & y<0)
+{
+Console.WriteLine("third quarter");
+}
+else if (x>0 & y <0)
+{
+Console.WriteLine("fourth quarter");
+}
+else 
+{
+Console.WriteLine("can't define quarter");
+}
