6fc7030f21eccc9a518f968804d37e4038e1edbd
Trintos <trintos@live.ru>
2022-03-21 22:20:15 +0300

    add task 25-30

---
+++ Introduction_to_C#/Lesson1/Task26/Program.cs
@@ -0,0 +1,5 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+Console.Write("Введите число: ");
+int Digit = Convert.ToInt32(Console.ReadLine());
+Digit = Math.Abs(Digit);
+Console.Write(Convert.ToString(Digit).Length);
