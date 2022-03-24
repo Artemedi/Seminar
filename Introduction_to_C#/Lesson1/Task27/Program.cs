db38bff47bebdef3ce817c7ab40ffbafc7725fe6
Trintos <trintos@live.ru>
2022-03-24 20:10:08 +0300

    update task

---
+++ Introduction_to_C#/Lesson1/Task27/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите число");
+
+int a = Convert.ToInt32(Console.ReadLine());
+int res = 0;
+int b = 0;
+while (a > 0)
+{
+    b = a % 10;
+    res += b;
+    a = a / 10;
+}
+Console.WriteLine($"Сумма цифр в числе будет равна {res}");
