bb3ec54c1d5c428624c945e0d7b2dd774620aa00
Trintos <trintos@live.ru>
2022-04-01 18:42:35 +0300

    Заполнение массива случайными числами и нахождение в нём чётных

---
+++ Introduction_to_C#/Lesson1/Task34/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве
+
+Console.WriteLine("Введите желаемое количество элементов массива");
+int perem = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+//Получаем из ввода число и путем ABS делаем его всего положительным
+int[] array = new int[perem];
+//Задаем длину массива из введеного числа
+int count = 0;
+Console.Write("Исходный массив: ");
+
+for (int i = 0; i < perem; i++)
+    {
+        array[i] = new Random().Next(100, 999);
+        Console.Write(array[i] + " ");
+    }
+Console.WriteLine();
+Console.Write("Чётные числа в массиве: ");
+for (int i = 0; i < perem; i++)
+{
+    if(array[i] % 2 == 0)
+    {
+        Console.Write(array[i] + " ");
+        count++;
+    }
+}
+Console.WriteLine();
+Console.WriteLine($"Количество чётных чисел в массиве: {count}");
+
