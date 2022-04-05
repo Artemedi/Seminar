e8c936cf8b8c5fa066e5c3ddaef8d100c30f24d8
Trintos <trintos@live.ru>
2022-04-05 13:58:34 +0300

    Трехназные положительные числа с выводов количества чётных

---
+++ Introduction_to_C#/Lesson1/Task36/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+Console.Write("Введите желаемое количество элементов массива: ");
+int[] array = new int[Math.Abs(Convert.ToInt32(Console.ReadLine()))]; //Задаем длину массива из введеного числа
+int perem = array.Length;
+int count = 0; 
+
+void gen_new_array()
+{
+    Random rnd = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(10, 99);
+    }
+}
+
+gen_new_array();
