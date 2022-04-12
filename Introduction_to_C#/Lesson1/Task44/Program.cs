e5ae42039b39375a3f19900d9b1fbcf4facda722
Trintos <trintos@live.ru>
2022-04-12 17:35:43 +0300

    Числа Фиббоначи без рекурсии

---
+++ Introduction_to_C#/Lesson1/Task44/Program.cs
@@ -0,0 +1,15 @@
+﻿//44. Не используя рекурсию, выведите первые N чисел Фибоначчи. Первые два числа Фибоначчи: 0 и 1.
+	double number = 1000;
+        double firstElem = 1;
+        double secondElem = 1;
+
+        Console.WriteLine($"1. {firstElem}");
+        Console.WriteLine($"2. {secondElem}");
+
+        for (int i = 3; i <= number; i++)
+        {
+            double nextElem = firstElem + secondElem;
+            Console.WriteLine($"{i}. {nextElem}");
+            firstElem = secondElem;
+            secondElem = nextElem;
+        }
