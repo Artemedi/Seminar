518039110bc3bd7a1cb74ce7e7cb3eb104540ccd
Trintos <trintos@live.ru>
2022-04-12 17:09:57 +0300

    Разница между максимальным и минимальным элементом массива

--- Introduction_to_C#/Lesson1/Task38/Program.cs
+++ Introduction_to_C#/Lesson1/Task38/Program.cs
@@ -51,3 +51,84 @@ void FindInArray(int[] FindInArray)
 gen_new_array();
 print_array(array);
 FindInArray(array);
+
+
+//38. Вариант с урока
+
+// double[] array = {1.7,3.6,2.7,8.9};
+// double[] Res(double[] diff){
+//   double max = diff[0];
+//   double min = diff[0];
+//   for(int i=0; i < diff.Length; i++){
+//     if(diff[i] > max){
+//       max = diff[i];
+//     }
+//     else if (diff[i] < min){
+//       min = diff[i];
+//     }
+//   }
+//   Console.WriteLine($"{max},{min}");
+//   return new double[] {max,min};
+// }
+// double[] newAr = Res(array);
+// Console.WriteLine($"{newAr[0]} - {newAr[1]} = {newAr[0]} - {newAr[1]}");
+
+// или так:
+
+// // Задача 38. Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+// using System.Text;
+
+// //Создание массива вещественных чисел в диапазоне от MinValue до MaxValue
+// double[] CreateDoubleArray(int Size, double MinValue = -10, double MaxValue = 10)
+// {
+//   double[] Result = new double[Size];
+
+//   Random genNumber = new Random();
+//   for (int i = 0; i < Size; i++)
+//   {
+//     Result[i] = MinValue + genNumber.NextDouble() * (MaxValue - MinValue);
+//   }
+
+//   return Result;
+// }
+
+// void PrintArray(double[] Array)
+// {
+//   String separator = ", ";
+//   StringBuilder stringBuilder = new StringBuilder("[");
+//   for (int i = 0; i < Array.Length - 1; i++)
+//   {
+//     stringBuilder.Append($"{Array[i]:f4}").Append(separator);
+//   }
+//   stringBuilder.Append($"{Array[Array.Length - 1]:f4}").Append("]");
+
+//   Console.WriteLine(stringBuilder.ToString());
+// }
+
+// double CalcMaxMinDiffrence(double[] Array)
+// {
+//   double MinValue = Array[0];
+//   double MaxValue = Array[0];
+
+//   for (int i = 1; i < Array.Length; i++)
+//   {
+//     if (Array[i] < MinValue)
+//     {
+//       MinValue = Array[i];
+//     }
+//     else if (Array[i] > MaxValue)
+//     {
+//       MaxValue = Array[i];
+//     }
+//   }
+
+//   return MaxValue - MinValue;
+// }
+
+// Console.Write("Введите размер массива: ");
+// int ArrSize = Convert.ToInt32(Console.ReadLine());
+// double[] Array = CreateDoubleArray(ArrSize);
+// PrintArray(Array);
+
+// double Diff = CalcMaxMinDiffrence(Array);
+// Console.WriteLine($"Разница между максимальным и минимальным элементами массива: {Diff:f4}");
