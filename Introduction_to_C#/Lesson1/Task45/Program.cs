28febcf259b496c4387f0c4f561e50f591526dce
Trintos <trintos@live.ru>
2022-04-14 14:27:18 +0300

    Создать поэлементно копию массива

---
+++ Introduction_to_C#/Lesson1/Task45/Program.cs
@@ -0,0 +1,32 @@
+﻿//Задача 45: Напишите программу, которая будет создавать копию заданного массива 
+//с помощью поэлементного копирования (программа копирования массива)
+
+Console.WriteLine("Введите длинну массива");
+int LenghtMass = Convert.ToInt32(Console.ReadLine());
+int[] Mass = new int[LenghtMass];
+int[] MassCopy; // = new int[LenghtMass];
+
+for (int k = 0; k < Mass.Length; k++)
+{
+    Mass[k] = new Random().Next(-100, 101);
+    Console.Write($" {Mass[k]}");
+}
+
+Console.WriteLine(" ");
+
+int[] Copy(int[] arr)
+{
+    int[] result = new int[arr.Length];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        result[i] = arr[i];
+    }
+    return result;
+}
+
+MassCopy = Copy(Mass);
+
+for (int j = 0; j < MassCopy.Length; j++)
+{
+    Console.Write($" {MassCopy[j]}");
+}
