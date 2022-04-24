866e3fb909b6183567317b62ff635e034c8a78af
Trintos <trintos@live.ru>
2022-04-24 21:02:04 +0300

    Произведение двух матриц

--- Introduction_to_C#/Lesson1/Task58/Program.cs
+++ Introduction_to_C#/Lesson1/Task58/Program.cs
@@ -23,6 +23,23 @@ void PrintArray(int[,] array)
     }
 }
 
+int[,] MatrixC(int[,] arrayA, int[,] arrayB)
+{
+    int[,] arrayC = new int[arrayA.GetLength(0), arrayB.GetLength(1)];
+    for (int i = 0; i < arrayA.GetLength(0); i++)
+    {
+        for (int j = 0; j < arrayB.GetLength(1); j++)
+        {
+            for (int k = 0; k < arrayA.GetLength(1); k++)
+            {
+                arrayC[i, j] = arrayC[i, j] + arrayA[i, k] * arrayB[k, j];
+            }
+        }
+    }
+    return arrayC;
+}
+
+
 
 
 Console.WriteLine("Введите количество строк двумерного массива: ");
@@ -38,12 +55,19 @@ Console.WriteLine($"Сгенерированный массив 1: ");
 PrintArray(ArrayOne);
 Console.WriteLine($"Сгенерированный массив 2: ");
 PrintArray(ArrayTwo);
+int[,] ResultMatrixC = MatrixC(ArrayOne, ArrayTwo);
+Console.WriteLine($"Произведение двух матриц:");
+PrintArray(ResultMatrixC);
 
-if (ArrayOne.GetLength(1) != ArrayTwo.GetLength(0))
-    {
-        Console.WriteLine("Матрицы невозможно перемножить в таком порядке!");
-    }
-else
-{
-    Console.WriteLine("Матрицы возможно перемножить в таком порядке!");
-}
+// if (ArrayOne.GetLength(1) != ArrayTwo.GetLength(0))
+//     {
+//         Console.WriteLine("Матрицы невозможно перемножить в таком порядке!");
+//     }
+// else
+// {
+//     Console.WriteLine("Матрицы возможно перемножить в таком порядке!");
+// }
+// Я не смог понять как бы мне сделать проверку на множитель. Точнее я понимаю, что можно через if, но я не знаю как остановить выполнение программы.
+// Пробовал делать и в методе и просто if и в проверке на ввод. Сообразить не могу как сделать правильно. Плюс массивы.
+// В итоге я просто всё жестко ограничил и сделал, что бы вторая матрица просто использовала теже параметры, что и первая.
+// Более того, я подсмотрел решение по вычислению матрицы т.к. уже говорил ранее - я полный профан в математике и даже такая база мне даётся с трудом.
